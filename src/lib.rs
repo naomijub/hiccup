ef407dfc7693a6cde0b7c2401f126480f90b4c0f
Julia Naomi <jnboeira@outlook.com>
2019-12-28 03:32:40 -0300

    Basic html

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
+
+#[macro_export]
+macro_rules! hiccup {
+    ($w:expr, ) => (());
+
+    ($w:expr, $e:tt) => {{
+        use std::fmt::Write;
+        let _ = write!($w, "{}", $e);
+    }};
+
+    ($w:expr, $tag:ident [$($inner:tt)*] $($rest:tt)*) => {{
+        use std::fmt::Write;
+        
+        let _ = write!($w, "<{}>", stringify!($tag));
+        hiccup!($w, $($inner)*);
+        let _ = write!($w, "</{}>", stringify!($tag));
+        hiccup!($w, $($rest)*);
+    }};
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn basic_html() {
+        let mut out = String::new();
+
+        hiccup!(&mut out,
+            html[
+                head[title["Macros guide"]]
+                body[h1["Macros are the best!"]]
+            ]);
+
+            assert_eq!(out, "<html><head><title>Macros guide</title></head>\
+            <body><h1>Macros are the best!</h1></body></html>");
     }
 }
